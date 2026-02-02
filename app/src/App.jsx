c4b40ba41b225de2cd609339674586dc43c8ba13
Vaasu Goyal <vaasugoyal16@gmail.com>
2026-02-02 16:20:02 +0530

    initial commit

---
+++ app/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+   <>
+    <h1>Hello </h1>
+    <h2>vg</h2>
+   </>
+  )
+}
+
+export default App
---
+++ fs_d1.js
@@ -0,0 +1,51 @@
+// {var a=10,b=21;       //let var const
+// console.log("addition is",a+b);
+// }
+
+// {var a=10,b=21;}         //let var const
+// console.log("addition is",a+b);
+
+// {const a=10,b=21;        //let var const
+// console.log("addition is",a+b);
+// }
+// var a=10;
+// console.log(a);
+// a=20;
+// console.log(a);
+// let-block scope and mutable
+//var -not have scope and mutable
+//const block scope and immutable
+// console.log(a);
+// var a=20;
+// let sum=0;
+// let i=0;
+// for (i=0;i<5;i++){
+//     if(i%2==0){
+//      sum=sum+i;
+//     }
+// }
+// console.log(sum);
+// // console.log(5=='5');//checks value in quotes-true
+// // console.log(5==='5');//checks datatype - false
+// let num=2;
+// let str='hello';
+// let arr=[1,2,3,4];
+// console.log(arr[0]);
+// let obj={name: "robin",age:10};
+// console.log(obj);
+// let a;
+// let emp=null;
+// let isok=true;
+// console.log(typeof(isok));
+
+function add(a,b){
+    return a+b;
+}
+console.log(add(5,8));
+function greet(){
+    console.log("hi");
+}
+greet();
+
+
+
